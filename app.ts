2e337a6de20f447ecdca49b0382c4ff4b50cd2d2
Punit_soni99 <punitsoni028@gmail.com>
2026-07-19 13:13:30 +0530

    Allow local frontend host in CORS

--- app.ts
+++ app.ts
@@ -7,9 +7,21 @@ import paymentRoutes from "./routes/payment.routes";
 
 const app: Application = express();
 
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  "http://localhost:5173",
+  "http://127.0.0.1:5173",
+];
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   }),
 );
